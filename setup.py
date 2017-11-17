31edaf3b758f03feb7146554e763f223befdbb96
dhilipsiva <dhilipsiva@gmail.com>
2017-11-17 13:17:59 +0530

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ds-gitric',
-    version='0.4',
+    version='0.4.1',
     description='A fork of gitric. With my customizations.',
     author='Dan Bravender',
     author_email='dan.bravender@gmail.com',
